ca806d4f3d6f19aac5c42fa8971d710064d8cac2
LAGonauta <lagonauta@gmail.com>
2023-02-12 12:06:35 -0300

    First try building dino with gtk4 on Windows

--- .ci/install-dependencies.sh
+++ .ci/install-dependencies.sh
@@ -4,7 +4,7 @@ pacman -S --needed --noconfirm \
        mingw64/mingw-w64-x86_64-cmake \
        mingw64/mingw-w64-x86_64-ninja \
        mingw64/mingw-w64-x86_64-libsoup \
-       mingw64/mingw-w64-x86_64-gtk3 \
+       mingw64/mingw-w64-x86_64-gtk4 \
        mingw64/mingw-w64-x86_64-sqlite3 \
        mingw64/mingw-w64-x86_64-gobject-introspection \
        mingw64/mingw-w64-x86_64-glib2 \
@@ -18,7 +18,7 @@ pacman -S --needed --noconfirm \
        mingw64/mingw-w64-x86_64-ntldd-git \
        mingw64/mingw-w64-x86_64-gpgme \
        mingw64/mingw-w64-x86_64-nsis \
-       mingw64/mingw-w64-x86_64-libhandy \
+       mingw64/mingw-w64-x86_64-libadwaita \
        mingw64/mingw-w64-x86_64-gspell \
        mingw64/mingw-w64-x86_64-enchant \
        mingw64/mingw-w64-x86_64-hunspell \
